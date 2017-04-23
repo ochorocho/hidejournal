de08f399e074c5295be1b6795b197a11c22c50b8
ochorocho <root@knallimall.org>
2017-04-23 13:38:45 +0200

    cleanup js

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -5,25 +5,20 @@ $(document).ready(function() {
 		var url = href[0];
 		var params = href[1];
 		
-		journal = $(this);
+		var journal = $(this);
 		
 		$.ajax({
 			dataType: "json",
 			url: url,
 			data: params,
 			success: function(data) {
-				// alert('succes');
 				var changed_to = data[0].changed_to;
 				var id = params.split('=');
 				if(changed_to == 'true') {
-					console.log(changed_to);
 					$('#change-' + id[1]).addClass('private-notes');
-					console.log($(this));
 					$(journal).removeClass("invisible").addClass("visible").attr('title', JOURNAL_ENABLE);
 				} else if(changed_to == 'false') {
-					console.log(changed_to);
 					$('#change-' + id[1]).removeClass('private-notes');
-					console.log($(this));
 					$(journal).removeClass("visible").addClass("invisible").attr('title', JOURNAL_DISABLE);
 				}
 			},
